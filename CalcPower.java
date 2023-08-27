5fc28e16291130914f6d2a1f7acddb2ad6937e00
SOOKRET <122726983+SOOKRET@users.noreply.github.com>
2023-08-27 11:28:33 +0530

    Create CalcPower.java

---
+++ CalcPower.java
@@ -0,0 +1,11 @@
+package org.calculator;
+
+public class CalcPower {
+    public static int square(int a) {
+        return a * a;
+    }
+
+    public static int cube(int a) {
+        return a * a * a;
+    }
+}
